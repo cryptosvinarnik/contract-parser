698640c63db5bd4ec4cdc8c862fe267103cf0768
cryptosvinarnik <na4030680@gmail.com>
2022-09-15 10:13:52 +0300

    🐽: local server + zip

---
+++ app_cmd.py
@@ -0,0 +1,17 @@
+from utils.scan_api import EVMScan
+
+from utils.saving import save_contract
+
+endpoints = {"1": "https://api.etherscan.io", "2": "https://api.bscscan.com", "3": "https://api.polygonscan.com"}
+
+
+if __name__ == "__main__":
+    API_KEY = ""  # <--- input your API_KEY from etherscan.io
+
+    endpoint =  input("Choise endpoint (1 - ether, 2 - bsc, 3 - polygon): ")
+    address = input("Input address of a verified smart contract: ")
+
+    try:
+        save_contract(EVMScan(API_KEY, endpoints[endpoint]), address)
+    except KeyboardInterrupt:
+        pass
