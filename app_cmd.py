5110b9c6f40ecce94f8fc4cf3bccbbebce8b8e4c
cryptosvinarnik <na4030680@gmail.com>
2022-11-27 08:22:03 +0200

    deploying to testnet

--- app_cmd.py
+++ app_cmd.py
@@ -1,17 +1,29 @@
+from loguru import logger
+
+from config import API_KEY, ENDPOINTS, SCANS
+from utils.deployment import deploy_contract
+from utils.saving import get_contract_bytecode, save_contract
 from utils.scan_api import EVMScan
-
-from utils.saving import save_contract
-
-endpoints = {"1": "https://api.etherscan.io", "2": "https://api.bscscan.com", "3": "https://api.polygonscan.com"}
 
 
 if __name__ == "__main__":
-    API_KEY = ""  # <--- input your API_KEY from etherscan.io
+    action = input("Select an action (1 - save the contract, 2 - deploy to testnet): ")
 
-    endpoint =  input("Choise endpoint (1 - ether, 2 - bsc, 3 - polygon): ")
+    endpoint =  input("Choise mainnet endpoint (1 - ether, 2 - bsc, 3 - polygon, 4 - rinkeby): ")
     address = input("Input address of a verified smart contract: ")
 
-    try:
-        save_contract(EVMScan(API_KEY, endpoints[endpoint]), address)
-    except KeyboardInterrupt:
-        pass
+    if action.strip() == "1":
+        save_contract(EVMScan(API_KEY, ENDPOINTS[endpoint]), address)
+
+    elif action.strip() == "2":
+        bytecode = get_contract_bytecode(SCANS[endpoint], address)
+
+        chain_id = int(input(f"Choose chain_id (rinkeby - 4 , goerli - 5, kovan - 42, ropsten - 3, bsc_testnet - 97): "))
+        rpc = input("Enter the RPC http: ")
+        private_key = input("Enter the private key: ")
+        gas_limit = int(input("Enter the gas limit: "))
+        gwei = float(input("Enter the value of gwei: "))
+
+        txn_hash = deploy_contract(rpc, private_key, bytecode, chain_id, gas_limit, gwei)
+
+        logger.success(f"Hash: {txn_hash}")
---
+++ config.py
@@ -0,0 +1,6 @@
+API_KEY = ""  # <--- input your API_KEY from etherscan.io
+
+
+# DON'T TOUCH THIS
+ENDPOINTS = {"1": "https://api.etherscan.io", "2": "https://api.bscscan.com", "3": "https://api.polygonscan.com", "4": "https://api-rinkeby.etherscan.io/"}
+SCANS = {"1": "https://etherscan.io", "2": "https://bscscan.com", "3": "https://polygonscan.com", "4": "https://rinkeby.etherscan.io/"}
